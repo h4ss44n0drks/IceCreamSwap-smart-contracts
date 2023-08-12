3b50e829f20085eba4608b243926d492220c2177
simon <simon@klein-homepage.de>
2023-08-12 14:08:16 +0300

    added ts types and abi for token deployer

--- projects/token-deployer/hardhat.config.ts
+++ projects/token-deployer/hardhat.config.ts
@@ -1,4 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
+import "@typechain/hardhat";
+import "hardhat-abi-exporter";
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -13,6 +15,14 @@ const config: HardhatUserConfig = {
         },
       },
     ],
+  },
+  typechain: {
+    outDir: "typechain",
+    target: "ethers-v5",
+  },
+  abiExporter: {
+    runOnCompile: true,
+    clear: true,
   },
 };
 
