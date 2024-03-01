7a0f1af6170c5b0c06714d9bead8b4183a493ddc
simon <simon@klein-homepage.de>
2024-03-01 15:17:57 +0200

    now awaiting deployments to be mined so block explorer verification work.

--- projects/common/src/deployAndVerify.ts
+++ projects/common/src/deployAndVerify.ts
@@ -4,6 +4,7 @@ import { tryVerify } from "./verify";
 export const deployAndVerify = async (contractName: string, constructorArguments: any[] = []) => {
   const ContractFactory = await ethers.getContractFactory(contractName);
   const deployedContract = await ContractFactory.deploy(...constructorArguments);
+  await deployedContract.waitForDeployment()
   console.log(`${contractName} deployed to ${deployedContract.target.toString()}`);
   await tryVerify(deployedContract.target, constructorArguments);
   return deployedContract;
