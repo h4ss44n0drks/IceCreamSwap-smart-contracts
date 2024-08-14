1f24e2c877c7518070f6c40f50b79ed340ea368d
simon <simon@klein-homepage.de>
2024-08-14 14:08:17 +0200

    combined MulticallV3 with PanCakeSwap UI multicall to multicallCombined.sol

--- projects/multicall/scripts/deploy.ts
+++ projects/multicall/scripts/deploy.ts
@@ -4,12 +4,12 @@ import { writeFileSync } from "fs";
 async function main() {
   const { chainName } = await getChainConfig();
 
-  const multicall = await deployAndVerify("Multicall3", []);
+  const multicall = await deployAndVerify("MulticallCombined", []);
 
   const contracts = {
     multicall: multicall.target,
   };
-  writeFileSync(`./deployments/${chainName}.json`, JSON.stringify(contracts, null, 2));
+  writeFileSync(`./deployments/${chainName}_combined.json`, JSON.stringify(contracts, null, 2));
 }
 
 // We recommend this pattern to be able to use async/await everywhere
